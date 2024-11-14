91571280aeb74ae6618f6015ebfb98910c40a670
Alexsandr Ermokov <Tyuiu.ErmakovA.A@outlook.com>
2024-11-14 06:27:29 +0500

    Task2

--- Tyuiu.ErmakovAA.Sprint4.Task2.V6.Lib/DataService.cs
+++ Tyuiu.ErmakovAA.Sprint4.Task2.V6.Lib/DataService.cs
@@ -6,13 +6,13 @@ namespace Tyuiu.ErmakovAA.Sprint4.Task2.V6.Lib
         public int Calculate(int[] array)
         {
 
-            int sum = 0;
+            int sum = 1;
          
             for (int i = 0; i <= array.Length - 1; i++)
             {
                 if (array[i] % 2 != 0)
                 {
-                    sum += array[i];
+                    sum *= array[i];
                 }
             }
             return sum;
