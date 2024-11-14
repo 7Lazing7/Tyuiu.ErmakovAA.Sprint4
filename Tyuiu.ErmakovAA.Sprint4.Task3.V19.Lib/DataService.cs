94b7263c94dd095928c60487b125f982b5cc429e
Alexsandr Ermokov <Tyuiu.ErmakovA.A@outlook.com>
2024-11-14 11:12:07 +0500

    Task3

--- Tyuiu.ErmakovAA.Sprint4.Task3.V19.Lib/DataService.cs
+++ Tyuiu.ErmakovAA.Sprint4.Task3.V19.Lib/DataService.cs
@@ -6,23 +6,23 @@ namespace Tyuiu.ErmakovAA.Sprint4.Task3.V19.Lib
         public int Calculate(int[,] array)
         {
 
-            int rows = array.GetUpperBound(0) +  1; // количество строк
+            int rows = array.GetUpperBound(0) + 1 ; // количество строк
             int cols = array.Length / rows; // количество столбцов
 
             int count = 0; 
 
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < rows; i++)  
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    if (array[i,j] % 2 != 0)
+                    if (array[i,j] % 2 == 0)
                     {
                         count++;
                     }
                 }
                 
             }
-            return count;
+            return  count;
         }
     }
 }
